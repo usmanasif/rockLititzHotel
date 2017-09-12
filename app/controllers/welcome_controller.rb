fca6aac5936ac6291009c09dd6abf13dc18a55d0
im genius <mail.usmanasif@gmail.com>
2017-09-12 10:02:29 +0500

    Meeting task done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,3 @@
 class WelcomeController < ApplicationController
 
-	def index
-		
-	end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "welcome#index"
+
+  get 'rooms', to: "welcome#rooms"
+  get 'amenities', to: "welcome#amenities"
+
 end
